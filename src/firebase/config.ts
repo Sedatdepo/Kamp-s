1a4effa81a7de1452e91fc68ab92edfef1533f91
Alterna Tarih <alternatarih@gmail.com>
2025-12-28 10:15:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5963979644-86cc8",
+  "appId": "1:201902779282:web:4cfb0cd6cc70f30a0d6f4b",
+  "apiKey": "AIzaSyDQ2t1391GhPQXHMpkRpyZKhy8bZoFKaXc",
+  "authDomain": "studio-5963979644-86cc8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "201902779282"
+};
