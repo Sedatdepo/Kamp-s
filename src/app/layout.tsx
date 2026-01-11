c5d34339f806997e878f8629a5efb8c2e0f02eb4
Alterna Tarih <alternatarih@gmail.com>
2026-01-11 08:10:11 +0000

    [Fast Refresh] done in 5011ms ?code=IRWQBZ&invite=true&tab=student:1 Acc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'İTO KAMPÜS',
   description: 'Lise öğretmenleri için dijital bir asistan ve öğrenciler için bir portal.',
-  manifest: '/manifest.json'
 };
 
 export default function RootLayout({
@@ -21,8 +20,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;600;700&display=swap" rel="stylesheet" />
-        <meta name="theme-color" content="#ffffff" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
       </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>
