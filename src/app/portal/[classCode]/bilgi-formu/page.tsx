f8ea0730c67c248ddad0685c2a0c09036af62efb
Alterna Tarih <alternatarih@gmail.com>
2026-04-02 18:09:39 +0000

    Hata var

--- src/app/portal/[classCode]/bilgi-formu/page.tsx
+++ src/app/portal/[classCode]/bilgi-formu/page.tsx
@@ -21,6 +21,7 @@ import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { Header } from '@/components/dashboard/Header';
+import { Label } from '@/components/ui/label';
 
 
 const siblingSchema = z.object({
